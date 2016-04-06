7fb7d6c65cfb7c04e4cb8b2093ade5d25f0c055d
Bruno Ferme Gasparin <brunogasparin@iMac-Bruno.local>
2016-04-05 22:38:11 -0300

    Improved PgNews

--- resources/assets/js/components/materialize-css/pgNews.vue
+++ resources/assets/js/components/materialize-css/pgNews.vue
@@ -1,7 +1,7 @@
 <template>
     <a href="{{ link }}">
         <div class="news" :class="{ 'z-depth-1' : hoverable, 'hoverable' : hoverable}" >
-            <div class="icon-block">
+            <div class="icon-block content">
                   <h3 class="center brown-text"><i class="material-icons">{{ icon }}</i></h3>
                   <h5 class="center">{{ title }}</h5>
                   <p class="light center"> {{ description }}</p>
@@ -37,4 +37,7 @@ export default {
         position: relative; 
         color: #000;
     } 
+    .content {
+        padding: 0.1rem 1rem;
+    }     
 </style>
