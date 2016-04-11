6f063b03284b634c1c88832d079c5d15e36ba257
Bruno Ferme Gasparin <brunogasparin@iMac-Bruno.local>
2016-04-11 17:19:53 -0300

    Moved project to Pai Galinha website

---
+++ resources/views/comming-soon.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Pai Galinha</title>
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body class="teal">
+    <div class="center">
+        <div class="row">
+            <div class="col s12" >
+                <div class="center-align">
+                  <h3 class="amber-text">Pai Galinha</h3>
+                  <h5 class="light lime-text text-lighten-3">(Em breve)</h5>
+                </div>
+                <img class="responsive-img" src="images/cristal_ball.png">
+            </div>      
+        </div>
+    </div>
+    <!-- JavaScripts -->
+    <script src="{{ elixir('js/app.js') }}"></script>
+</body>
+</html>
